6b352d442a76157ab0b5f7889a136b70bd58773d
absinghd <absinghd@gmail.com>
2024-02-12 15:54:27 -0800

    new repo setup

---
+++ little-lemon-webapp/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import Main from './Main'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Nav />
+
+      <Main />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
